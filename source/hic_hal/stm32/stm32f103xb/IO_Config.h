ac23f8195b1d7cc27aa36e572caa578c3ffd55cc
jerricjiang <jerricjiang@qq.com>
2022-05-05 23:29:54 +0800

    stm32/stm32f103xb/IO_Config.h //nRESET OUT Pin更改到ZX_DAPLink 的fw更新按键S1。 //只用于作强制进入 bootloader mode的按键，并没有用于做Target Reset。

--- source/hic_hal/stm32/stm32f103xb/IO_Config.h
+++ source/hic_hal/stm32/stm32f103xb/IO_Config.h
@@ -37,50 +37,50 @@ COMPILER_ASSERT(DAPLINK_HIC_ID == DAPLINK_HIC_ID_STM32F103XB);
 #define USB_CONNECT_OFF()            (USB_CONNECT_PORT->BRR  = USB_CONNECT_PIN)
 
 //Connected LED
-#define CONNECTED_LED_PORT           GPIOC	//GPIOB
-#define CONNECTED_LED_PIN            GPIO_PIN_15	//GPIO_PIN_6
-#define CONNECTED_LED_PIN_Bit        15	//6
+#define CONNECTED_LED_PORT           GPIOC  //GPIOB
+#define CONNECTED_LED_PIN            GPIO_PIN_15    //GPIO_PIN_6
+#define CONNECTED_LED_PIN_Bit        15 //6
 
 //When bootloader, disable the target port(not used)
-#define POWER_EN_PIN_PORT            GPIOC	//GPIOB
+#define POWER_EN_PIN_PORT            GPIOC  //GPIOB
 #define POWER_EN_PIN                 GPIO_PIN_14
 #define POWER_EN_Bit                 14
 
-// nRESET OUT Pin
-#define nRESET_PIN_PORT              GPIOB
-#define nRESET_PIN                   GPIO_PIN_2	//GPIO_PIN_0
-#define nRESET_PIN_Bit               2	//0
+// nRESET OUT Pin   //只用于作强制进入 bootloader mode的按键，并没有用于做Target Reset。
+#define nRESET_PIN_PORT              GPIOC
+#define nRESET_PIN                   GPIO_PIN_13    //GPIO_PIN_0
+#define nRESET_PIN_Bit               13 //0
 
 //SWD
 #define SWCLK_TCK_PIN_PORT           GPIOB
-#define SWCLK_TCK_PIN                GPIO_PIN_6	//GPIO_PIN_13
-#define SWCLK_TCK_PIN_Bit            6	//13
+#define SWCLK_TCK_PIN                GPIO_PIN_6 //GPIO_PIN_13
+#define SWCLK_TCK_PIN_Bit            6  //13
 
 #define SWDIO_OUT_PIN_PORT           GPIOB
-#define SWDIO_OUT_PIN                GPIO_PIN_7	//GPIO_PIN_14
-#define SWDIO_OUT_PIN_Bit            7	//14
+#define SWDIO_OUT_PIN                GPIO_PIN_7 //GPIO_PIN_14
+#define SWDIO_OUT_PIN_Bit            7  //14
 
 #define SWDIO_IN_PIN_PORT            GPIOB
-#define SWDIO_IN_PIN                 GPIO_PIN_9	//GPIO_PIN_12
-#define SWDIO_IN_PIN_Bit             9	//12
+#define SWDIO_IN_PIN                 GPIO_PIN_9 //GPIO_PIN_12
+#define SWDIO_IN_PIN_Bit             9  //12
 
 //LEDs
 //USB status LED
 #define RUNNING_LED_PORT             GPIOA
-#define RUNNING_LED_PIN              GPIO_PIN_1	//GPIO_PIN_9
-#define RUNNING_LED_Bit              1	//9
+#define RUNNING_LED_PIN              GPIO_PIN_1 //GPIO_PIN_9
+#define RUNNING_LED_Bit              1  //9
 
 #define PIN_HID_LED_PORT             GPIOA
-#define PIN_HID_LED                  GPIO_PIN_1	//GPIO_PIN_9
-#define PIN_HID_LED_Bit              1	//9
+#define PIN_HID_LED                  GPIO_PIN_1 //GPIO_PIN_9
+#define PIN_HID_LED_Bit              1  //9
 
 #define PIN_CDC_LED_PORT             GPIOA
-#define PIN_CDC_LED                  GPIO_PIN_1	//GPIO_PIN_9
-#define PIN_CDC_LED_Bit              1	//9
+#define PIN_CDC_LED                  GPIO_PIN_1 //GPIO_PIN_9
+#define PIN_CDC_LED_Bit              1  //9
 
 #define PIN_MSC_LED_PORT             GPIOA
-#define PIN_MSC_LED                  GPIO_PIN_1	//GPIO_PIN_9
-#define PIN_MSC_LED_Bit              1	//9
+#define PIN_MSC_LED                  GPIO_PIN_1 //GPIO_PIN_9
+#define PIN_MSC_LED_Bit              1  //9
 
 
 #endif
